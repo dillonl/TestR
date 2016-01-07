bc8a214966f20668f4963832adea3df263246dcc
dillonl <dlee@genetics.utah.edu>
2016-01-07 10:01:12 -0700

    the set hashing algorithm concept is working, now you need to architect it

--- core/parsers/AlignmentParser.hpp
+++ core/parsers/AlignmentParser.hpp
@@ -17,40 +17,41 @@ namespace rufus
 	{
 	public:
 		template< uint32_t KMER_SIZE = 25 >
-		static inline bool ParseAlignment(const char* alignment, size_t alignmentLength)
+		static inline bool ParseAlignment(const char* alignment, size_t alignmentLength, std::unordered_set< InternalKmer, KmerHash, KmerKeyEqual >* kSet)
 		{
-			InternalKmer internalKmer = 0; // we must guarentee internalKmer is zero
-			/*
-			auto charParser = [&](const char* alignmentCharacter, InternalKmer& kmer, uint64_t kmerBitOffset) -> bool
-			{
-
-
-				// uint64_t a = (alignmentCharacter[kmerBitOffset] >> 1);
-				//uint64_t b = ((alignmentCharacter[kmerBitOffset] >> 1) & 3) << (kmerBitOffset * 2);
-				uint64_t shifter = (kmerBitOffset * 2);
-				if ((alignmentCharacter[kmerBitOffset]) & (1 >> (3))) { return false; } // if this is a non basepair char
-				kmer |= ((alignmentCharacter[kmerBitOffset] >> shifter1) & shifter3) << shifter;
-
-				// std::cout << "shifting by: " << kmerBitOffset * 2 << std::endl;
-				// std::cout << "test1: " << alignmentCharacter[kmerBitOffset] << " " << std::bitset< 64 >(alignmentCharacter[kmerBitOffset]) << std::endl;
-				//std::cout << "test2: " << alignmentCharacter[kmerBitOffset] << " " << std::bitset< 8 >(a) << std::endl;
-				// std::cout << "test3: " << alignmentCharacter[kmerBitOffset] << " " << std::bitset< 64 >(kmer) << std::endl;
-				// std::cout << std::endl;
-				return true;
-			};
-			*/
-			std::vector< InternalKmer > kmers;
+			std::cout << "addr: " << kSet << std::endl;
 			auto kmerIterations = (alignmentLength - KMER_SIZE);
 			for (auto i = 0; i <= kmerIterations; ++i)
 			{
+				InternalKmer internalKmer = 0; // we must guarentee internalKmer is zero
 				bool validKmer = true;
-				// validKmer &= unroller(charParser, i, uint_< KMER_SIZE - 1 >(),  alignment, internalKmer);
 				validKmer &= unroller(i, uint_< KMER_SIZE - 1 >(),  alignment, internalKmer);
-				kmers.push_back(internalKmer);
-				// queue.push(internalKmer);
-				// std::cout << validKmer << " " << "----" << std::endl;
+
+				std::string kmer = std::string(alignment + i, 25);
+				auto iter = kSet->find(internalKmer);
+				if (iter != kSet->end())
+				{
+					uint64_t fiftyShift = 50;
+					auto iterPtr = const_cast< InternalKmer* >(&(*iter));
+					uint64_t count = (((*iter) >> fiftyShift) + std::uint64_t(1));
+					*iterPtr = internalKmer | (count << fiftyShift);
+					iter = kSet->find(internalKmer);
+					std::cout << "found: " << kmer << " " << std::bitset< 64 >(*iter) << " " << std::bitset< 8 >(count) << " " << count << std::endl;
+				}
+				else
+				{
+					// std::cout << "k1:    " << kmer << " " << std::bitset< 64 >(internalKmer) << " " << internalKmer << std::endl;
+					kSet->emplace(internalKmer);
+					iter = kSet->find(internalKmer);
+					auto iterPtr = const_cast< InternalKmer* >(&(*iter));
+					*iterPtr = internalKmer | 0x0004000000000000;
+					// std::cout << "k2:    " << kmer << " " << std::bitset< 64 >(*iter) << std::endl;
+					iter = kSet->find(internalKmer);
+					std::cout << "k3:    " << kmer << " " << std::bitset< 64 >(*iter) << " " << internalKmer << std::endl;
+				}
 				if (!validKmer) { return false;	}
 			}
+			std::cout << "---" << std::endl;
 			return true;
 		}
 
@@ -63,31 +64,27 @@ namespace rufus
 
 		template <size_t N> struct uint_{ };
 
-		// template <size_t N, typename Lambda, typename IterT>
-		// static inline bool unroller(const Lambda& f, const IterT& iter, uint_<N>, const char* alignment, InternalKmer& internalKmer)
 		template <size_t N, typename IterT>
 		static inline bool unroller(const IterT& iter, uint_<N>, const char* alignment, InternalKmer& internalKmer)
 		{
-			// unroller(f, iter, uint_<N-1>(), alignment, internalKmer);
 			unroller(iter, uint_<N-1>(), alignment, internalKmer);
-			// return f(alignment, internalKmer, iter + N);
-			return parse(alignment, internalKmer, iter + N);
+			// return parse(alignment, internalKmer, iter + N);
+			return parse(alignment, internalKmer, N, iter);
 		}
 
-		// template <typename Lambda, typename IterT>
-		// static inline bool unroller(const Lambda& f, const IterT& iter, uint_<0>, const char* alignment, InternalKmer& internalKmer)
 		template <typename IterT>
 		static inline bool unroller(const IterT& iter, uint_<0>, const char* alignment, InternalKmer& internalKmer)
 		{
 			// return f(alignment, internalKmer, iter + 0);
-			return parse(alignment, internalKmer, iter + 0);
+			return parse(alignment, internalKmer, 0, iter);
 		}
 
-		static inline bool parse(const char* alignmentCharacter, InternalKmer& kmer, uint64_t kmerBitOffset)
+		static inline bool parse(const char* alignmentCharacter, InternalKmer& kmer, uint64_t kmerBitOffset, uint64_t counter)
 		{
 			uint64_t shifter = (kmerBitOffset * 2);
-			if ((alignmentCharacter[kmerBitOffset]) & (1 >> (3))) { return false; } // if this is a non basepair char
-			kmer |= ((alignmentCharacter[kmerBitOffset] >> shifter1) & shifter3) << shifter;
+			// std::cout << "offset: " << alignmentCharacter[kmerBitOffset + counter] << " " << shifter << " " << counter << " " << std::bitset< 8 > (((alignmentCharacter[kmerBitOffset + counter] >> shifter1) & 0x3)) << std::endl;
+			if ((alignmentCharacter[kmerBitOffset + counter]) & (1 >> (3))) { return false; } // if this is a non basepair char
+			kmer |= (((alignmentCharacter[kmerBitOffset + counter] >> shifter1) & 0x3) & shifter3) << shifter;
 			return true;
 		}
 	};
--- tests/AlignmentParserTests.hpp
+++ tests/AlignmentParserTests.hpp
@@ -2,32 +2,59 @@
 #define TESTS_ALIGNMENTPARSER_H
 
 #include <string>
+#include <unordered_set>
 
 #include <boost/lockfree/queue.hpp>
 
+#include "hashers/KmerHash.hpp"
 #include "parsers/AlignmentParser.hpp"
 #include "utils/ThreadPool.hpp"
 
 namespace
 {
 	using namespace rufus;
+
+	/*
 	TEST(AlignmentParserTest, ParseA)
 	{
 		//std::string alignment = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		std::string alignment = "CCCCCCCCCCCCCCCCCCCCCCCCCAAAAAAAAAAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGGGGGGGGGGGTTTTTTTTTTTTTTTTTTTTTTTTT";
+		std::string alignment = "CCCCCCCCCCCCCCCCCCCCCCCCCA"; //AAAAAAAAAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGGGGGGGGGGGTTTTTTTTTTTTTTTTTTTTTTTTT";
 		InternalKmer parsedKmer;
 
-		std::unordered_map< InternalKmer, char > map;
+		std::unordered_set< InternalKmer, KmerHash, KmerKeyEqual > kSet;
 		ThreadPool::Instance()->setThreadCount(5);
 		std::cout << "starting" << std::endl;
 		bool success = true;
-		for (uint32_t i = 0; i < 100000000; ++i)
+		for (uint32_t i = 0; i < 1; ++i)
 		{
-			ThreadPool::Instance()->enqueue(std::bind(&AlignmentParser::ParseAlignment< 25 >, alignment.c_str(), alignment.size()));
+			ThreadPool::Instance()->enqueue(std::bind(&AlignmentParser::ParseAlignment< 25 >, alignment.c_str(), alignment.size(), kSet));
 			//success |= AlignmentParser::ParseAlignment(alignment.c_str(), alignment.size(), parsedKmer);
 		}
 		ThreadPool::Instance()->joinAll();
 		std::cout << "ending" << std::endl;
+		ASSERT_TRUE(success);
+		//ASSERT_EQ();
+	}
+	*/
+
+	TEST(AlignmentParserTest, HashTest)
+	{
+		//std::string alignment = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		// std::string alignment = "CCCCCCCCCCCCCCCCCCCCCCCCCAAAAAAAAAAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGGGGGGGGGGGTTTTTTTTTTTTTTTTTTTTTTTTT";
+		std::string alignment = "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC";
+		InternalKmer parsedKmer;
+
+		std::unordered_set< InternalKmer, KmerHash, KmerKeyEqual > kSet;
+		ThreadPool::Instance()->setThreadCount(1);
+		std::cout << "starting" << std::endl;
+		bool success = true;
+		for (uint32_t i = 0; i < 2; ++i)
+		{
+			ThreadPool::Instance()->enqueue(std::bind(&AlignmentParser::ParseAlignment< 25 >, alignment.c_str(), alignment.size(), &kSet));
+			//success |= AlignmentParser::ParseAlignment(alignment.c_str(), alignment.size(), parsedKmer);
+		}
+		ThreadPool::Instance()->joinAll();
+		std::cout << "ending: " << kSet.size() << std::endl;
 		ASSERT_TRUE(success);
 		//ASSERT_EQ();
 	}
