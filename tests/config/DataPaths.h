c51c66f06f8fc7bd79a68e301a44dc8fd962ca74
dillonl <dlee@genetics.utah.edu>
2016-01-12 13:55:17 -0700

    added data file

---
+++ tests/config/DataPaths.h
@@ -0,0 +1,7 @@
+#ifndef RUFUS_TESTS_DATAPATHS_H
+#define RUFUS_TESTS_DATAPATHS_H
+
+#define TEST_BAM_FILE "/Users/dillonl/Projects/RUFUS/tests/data/test.bam"
+#define TEST_BAM_INDEX_FILE "/Users/dillonl/Projects/RUFUS/tests/data/test.bam.bai"
+
+#endif //RUFUS_TESTS_DATAPATHS_H
--- tests/gtest_main.cpp
+++ tests/gtest_main.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 
+#include "config/DataPaths.h"
 #include "AlignmentParserTests.hpp"
 #include "SparseKmerSetTests.hpp"
 
