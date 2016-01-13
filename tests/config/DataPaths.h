ee5e1d5c1c2c6268be13032f6fffee17a68cf9d1
dillonl <dlee@genetics.utah.edu>
2016-01-12 19:16:11 -0700

    fixed a build issue and removed a autoconfig file from the commit

--- tests/config/DataPaths.h
+++
@@ -1,7 +0,0 @@
-#ifndef RUFUS_TESTS_DATAPATHS_H
-#define RUFUS_TESTS_DATAPATHS_H
-
-#define TEST_BAM_FILE "/Users/dillonl/Projects/RUFUS/tests/data/test.bam"
-#define TEST_BAM_INDEX_FILE "/Users/dillonl/Projects/RUFUS/tests/data/test.bam.bai"
-
-#endif //RUFUS_TESTS_DATAPATHS_H
