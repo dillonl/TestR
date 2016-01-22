c736b248db707c43095df87728e60f9f2138ae78
Dillon Lee <dlee123@gmail.com>
2016-01-22 11:29:23 -0700

    added a temporary test case

--- tests/BamAlignmentReaderTests.hpp
+++ tests/BamAlignmentReaderTests.hpp
@@ -33,6 +33,27 @@ namespace
 		threadPool->stop();
 		kmerSetManager->stopAndJoin();
 	}
+
+	TEST(BamAlignmentReaderTest, GetAllAlignmentsInRegion2)
+	{
+		ThreadPool::SharedPtr threadPool = std::make_shared< ThreadPool >();
+		KmerSetManager::SharedPtr kmerSetManager = std::make_shared< KmerSetManager >();
+		std::string path = "/uufs/chpc.utah.edu/common/home/marth-d1/data/WashU_serialTumors/bams/B0.bam";
+		std::vector< BamAlignmentReader::SharedPtr > readers;
+		auto regions = BamAlignmentReader::getAllRegionsInBam(path);
+		for (int i = 0; i < regions.size(); ++i)
+		{
+			auto thr = [&]()
+				{
+					BamAlignmentReader::SharedPtr reader = std::make_shared< BamAlignmentReader >(path, regions[i]);
+					reader->processAllReadsInRegion(kmerSetManager);
+					readers.emplace_back(reader);
+				};
+			threadPool->enqueue(thr);
+		}
+		threadPool->stop();
+		kmerSetManager->stopAndJoin();
+	}
 }
 
 
